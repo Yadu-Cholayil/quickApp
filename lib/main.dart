eac39d516a73c59c4ba72bdf10f0eea046d0c3f0
YaduCholayil <yadu1819@gmail.com>
2024-12-01 10:31:35 +0530

    login signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+
+const String PARSE_APP_ID = 'eqJQGZXIuclhvWtcEAMqa0JgpUyOu8DN67EawPWa';
+const keyClientKey = '7tl4bGVyPRz2LGvXsYZGHyyRPFfKA3g1tGYUVtkc';
+const String PARSE_APP_URL = 'https://parseapi.back4app.com';
+const String MASTER_KEY = 'NwOZvEUfcm6CPEssEvvcj13ETlRhPdBvyRFASuU8';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Parse().initialize(
+    PARSE_APP_ID,
+    PARSE_APP_URL,
+    masterKey: MASTER_KEY,  
+    autoSendSessionId: true,
+    debug: true,
+    coreStore: await CoreStoreSharedPreferences.getInstance(),
+  );
   runApp(const MyApp());
 }
 
@@ -11,115 +28,157 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Quick App'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Login'),
+                Tab(text: 'Signup'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              LoginCard(),
+              SignupCard(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginCard extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginCard> {
+  final controllerEmail = TextEditingController();
+  final controllerPassword = TextEditingController();
+  bool isLoggedIn = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Show success message
+  void showSuccess(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  // Show error message
+  void showError(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void doUserLogin() async {
+    final email = controllerEmail.text.trim();
+    final password = controllerPassword.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      showError("email and password cannot be empty!");
+      return;
+    }
+
+    // final user = ParseUser(email, password, null);
+
+    try {
+      var response = await user.login();
+
+      if (response.success) {
+        showSuccess("User was successfully logged in!");
+
+        setState(() {
+          isLoggedIn = true;
+        });
+      } else {
+        showError(response.error!.message);
+      }
+    } catch (e) {
+      // Catch any errors (e.g., network issues, server errors)
+      showError("An error occurred: ${e.toString()}");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text('Login Page')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: controllerEmail,
+              decoration: InputDecoration(labelText: 'Username'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 10),
+            TextField(
+              controller: controllerPassword,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: doUserLogin,
+              child: Text('Login'),
+            ),
+            if (isLoggedIn)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Logged in successfully!'),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add_circle_outline),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SignupCard extends StatelessWidget {
+  const SignupCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const TextField(
+                decoration: InputDecoration(labelText: 'Full Name'),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle signup logic here
+                },
+                child: const Text('Signup'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
